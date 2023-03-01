27b9d31f1aa579b015e2b1ab404b54c556246f4b
paulofujikawa <126348096+paulofujikawa@users.noreply.github.com>
2023-03-01 14:35:49 +0000

    lendo teclado

---
+++ lendo_teclado.py
@@ -0,0 +1,9 @@
+variavel_a = 20
+variavel_b = 5
+meu_nome = "paulo"
+print(variavel_a+variavel_b)
+print(variavel_a-variavel_b)
+print(variavel_a*variavel_b)
+print(variavel_a/variavel_b)
+print("meu nome é " + meu_nome )
+      
---
+++ lendoteclado2.py
@@ -0,0 +1,2 @@
+variavel_nome = input("qual e seu nome? ")
+print("meu nome e " + variavel_nome)
