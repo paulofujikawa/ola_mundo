e47444f20f18468da844a6dabd23e77ed399ae30
paulofujikawa <126348096+paulofujikawa@users.noreply.github.com>
2023-03-13 23:46:22 +0000

    ex05.py

---
+++ ex05.py
@@ -0,0 +1,6 @@
+valor = int(input("digite o valor"))
+desconto = (10/100)*valor
+valor_com_desconto = valor - desconto
+print(f"valor total é: {valor}")
+print(f"valor da porcentagem é: {desconto}")
+print(f"valor total com desconto é: {valor_com_desconto}")
---
+++ teste.py
@@ -0,0 +1,5 @@
+a = int(input("digite o valor"))
+b = 10
+resultado_porcentagem = (10/100)*a
+
+print(f"o resultado  é: {resultado_porcentagem}")
