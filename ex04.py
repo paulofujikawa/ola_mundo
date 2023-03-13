8d2dd2bd1310fa719a726a87d902f6f8f483122a
paulofujikawa <126348096+paulofujikawa@users.noreply.github.com>
2023-03-13 20:10:03 +0000

    ex04.py

---
+++ ex04.py
@@ -0,0 +1,6 @@
+valor = int(input("digite o valor" ))
+desconto = 10.00
+
+print(f"o valor total é: {valor}")
+print(f"o valor do desconto é: {desconto}")
+print(f"o valor total com desconto é: {valor - desconto}")
